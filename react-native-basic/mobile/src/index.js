8d0cdc5c3a473295692fc8d5bd8ef91953075748
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2020-07-01 02:59:38 -0300

    List repositories in emulator

---
+++ react-native-basic/mobile/src/index.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
+
+
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
+  function all_repos() {
+    console.log(repositories);
+  }
+
+  return (
+    <FlatList
+      data={repositories}
+      keyExtractor={repository => repository.id}
+      renderItem={({ item: repository }) => (
+        <View style={styles.container}>
+          <Text style={styles.project}>Repositório: {repository.title}</Text>
+          <Text style={styles.tech}>Tecnologias:</Text>
+        {repository.techs.map(tech => (
+          <Text key={tech} style={styles.tech}>
+            {tech}
+          </Text>
+        ))}
+        </View>
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  project: {
+    color: '#DDD',
+    fontWeight: 'bold',
+    fontSize: 32,
+  },
+  tech: {
+    color: '#FFF',
+    fontSize: 20
+  },
+  button: {
+    backgroundColor: '#000',
+    margin: 20,
+    height: 50,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    padding: 10,
+    color: '#DDD',
+  }
+})
+
+  // < TouchableOpacity style = { styles.button } >
+  //   <Text style={styles.buttonText}>Adicionar repositório</Text>
+  //         </TouchableOpacity >
