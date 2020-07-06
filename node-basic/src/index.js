4c74052e746d44a5ce95d1e207eba73e370f71c5
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2020-07-06 17:31:38 -0300

    Add CORS

--- node-basic/src/index.js
+++ node-basic/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.listen(3333);
 
 const repositories = [];
