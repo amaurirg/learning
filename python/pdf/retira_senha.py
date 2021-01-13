9e395a1354f53d092e0716abdb8be62debe37a62
Amauri <amaurirg@terra.com.br>
2021-01-13 13:26:13 -0200

    Descripted PDF files

---
+++ python/pdf/retira_senha.py
@@ -0,0 +1,21 @@
+#! python3
+# Se o arquivo PDF possuir uma senha, a informa para acessá-lo.
+
+import PyPDF2
+
+arquivo = ''
+senha = ''
+
+pdfReader = PyPDF2.PdfFileReader(open(arquivo, 'rb'))
+pdfReader.decrypt(senha)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+for pagenum in range(pdfReader.numPages):
+    pageobj = pdfReader.getPage(pagenum)
+    pdfWriter.addPage(pageobj)
+
+nome_arquivo = input("Digite um nome para criar o arquivo PDF: ")
+if not nome_arquivo.endswith('.pdf'):
+    nome_arquivo += '.pdf'
+with open(nome_arquivo, 'wb') as pdfOutput:
+    pdfWriter.write(pdfOutput)
---
+++ python/pdf/unePDFs.py
@@ -0,0 +1,32 @@
+# Necessário instalar o PyPDF2
+# pip install PyPDF2
+
+# Esse script gera um novo arquivo unindo o conteúdo de
+# cada arquivo PDF contido nessa pasta por ordem alfabética
+
+# OS ARQUIVOS EXISTENTES DESSA PASTA NÃO SERÃO ALTERADOS OU EXCLUÍDOS
+
+"""
+Como usar:
+Coloque todos os arquivos PDF e também esse script em uma pasta separada
+Execute esse script. Será solicitado um nome para o arquivo PDF a ser criado
+"""
+
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+pdfWriter = PdfFileWriter()
+filename = 'uniclass.pdf'
+pdfFileObj = PdfFileReader(open(filename, 'rb'))
+# pdfFileObj.isEncrypted
+pdfFileObj.decrypt('11748')
+pdfReader = PdfFileReader(pdfFileObj)
+for pagenum in range(pdfReader.numPages):
+    pageobj = pdfReader.getPage(pagenum)
+    pdfWriter.addPage(pageobj)
+
+nome_arquivo = input("Digite um nome para criar o arquivo PDF: ")
+if not nome_arquivo.endswith('.pdf'):
+    nome_arquivo += '.pdf'
+with open(nome_arquivo, 'wb') as pdfOutput:
+    pdfWriter.write(pdfOutput)
