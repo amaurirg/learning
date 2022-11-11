17d8ac4a5b022489bc541308771d12ee8aa0baef
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2022-11-11 20:49:40 -0300

    Deleted files

--- auth_unisa/unisa.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-
-
-b = webbrowser.get('google-chrome')
-b.open('https://w3.unisa.br/login/')
--- karol/test_exercicio2.py
+++
@@ -1,5 +0,0 @@
-from karol.exercicio2 import soma
-
-
-def test_soma():
-    assert soma(12, 23) == 35
